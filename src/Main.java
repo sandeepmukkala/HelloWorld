551b8f4bf94f30bccdbd0da2ca78991f9d49d272
sandeepmukkala <sandeepmukkala@gmail.com>
2018-04-29 02:30:55 -0400

    Add one more System.oi line

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("My First Hello World Project in IntelliJ IDEA");
+        System.out.println("Intial Update");
     }
 }
