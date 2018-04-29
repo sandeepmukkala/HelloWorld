980a4dae16d55982d856a0ce8e9d58e76e123ade
sandeepmukkala <sandeepmukkala@gmail.com>
2018-04-29 02:19:59 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("My First Hello World Project in IntelliJ IDEA");
+    }
+}
